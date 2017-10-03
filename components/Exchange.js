0be7db2283a72a440779996c5b53509e0c116f44
Oskar Hane <oh@oskarhane.com>
2017-10-03 22:27:57 +0200

    Don’t rerender Exchange unless needed

--- components/Exchange.js
+++ components/Exchange.js
@@ -1,22 +1,34 @@
-import React from "react";
+import React, { Component } from "react";
 import Fetch from "./Fetch";
 
-const Exchange = ({ from, to, sum = 1 }) => (
-  <Fetch
-    url={`https://min-api.cryptocompare.com/data/price?fsym=${from}&tsyms=${to}`}
-    handleRes={res => res.json()}
-    render={({ pending, error, res }) => {
-      return pending
-        ? "pending"
-        : error
-          ? error
-          : res
-            ? `${Number(Math.round(res[to] * sum)).toLocaleString(
-                "sv-SE"
-              )} ${to}`
-            : "wait";
-    }}
-  />
-);
+class Exchange extends Component {
+  shouldComponentUpdate(props) {
+    return !(
+      props.to === this.props.to &&
+      props.from === this.props.from &&
+      props.sum === this.props.sum
+    );
+  }
+  render() {
+    const { from, to, sum = 1 } = this.props;
+    return (
+      <Fetch
+        url={`https://min-api.cryptocompare.com/data/price?fsym=${from}&tsyms=${to}`}
+        handleRes={res => res.json()}
+        render={({ pending, error, res }) => {
+          return pending
+            ? "pending"
+            : error
+              ? error
+              : res
+                ? `${Number(Math.round(res[to] * sum)).toLocaleString(
+                    "sv-SE"
+                  )} ${to}`
+                : "wait";
+        }}
+      />
+    );
+  }
+}
 
 export default Exchange;
